e1ca9e7a80c0c9f0edbe3c74cf49dcd1c7f07f0f
skyclouds2001 <skyclouds2001@163.com>
2023-02-03 14:06:16 +0800

    build: change config file to javascript

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleAttributePerLine: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  vueIndentScriptAndStyle: false
+}
---
+++ .stylelintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order', 'stylelint-config-prettier'],
+  rules: {},
+  plugins: ['stylelint-order'],
+}
