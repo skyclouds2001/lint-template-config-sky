c8999c72fb812cdd479c6749d023bfe7940632f3
skyclouds2001 <skyclouds2001@163.com>
2023-02-18 23:52:04 +0800

    build: remove stylelint-order plugin config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   rules: {},
-  plugins: ['stylelint-order'],
+  plugins: [],
   overrides: [
     {
       files: ['**/*.md'],
